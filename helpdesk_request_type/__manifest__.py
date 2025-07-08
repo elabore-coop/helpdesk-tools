cdab2cbadff911311fddf627547e4ab9a439f29a
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2025-07-08 14:35:24 +0200

    [I18N]helpdesk_request_type

--- helpdesk_request_type/__manifest__.py
+++ helpdesk_request_type/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "helpdesk_request_type",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
