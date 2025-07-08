e04595b65d0ffbec75db9c51bc7b944eced1609f
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2025-07-08 14:36:48 +0200

    [I18N]helpdesk_portal_ticket_enriched_description_form

--- helpdesk_portal_ticket_enriched_description_form/__manifest__.py
+++ helpdesk_portal_ticket_enriched_description_form/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "helpdesk_portal_ticket_enriched_description_form",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
